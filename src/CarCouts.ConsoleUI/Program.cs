6bd09c66f32c87a9b2344d3c941788d08dc9601a
Everton J Benedicto <everton.benedicto@itsector.pt>
2022-11-12 11:10:57 +0000

    Exemplo de Encapsulamento.

--- src/CarCouts.ConsoleUI/Program.cs
+++ src/CarCouts.ConsoleUI/Program.cs
@@ -1,10 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CarCouts.OrientacaoObjeto.Encapsulamento;
 using CarCouts.OrientacaoObjeto.Heranca;
 using CarCouts.OrientacaoObjeto.Heranca.EX01;
 
-Console.WriteLine("-------------------------------------------");
-Console.WriteLine("Veiculos");
-Console.WriteLine("-------------------------------------------");
+//Console.WriteLine("-------------------------------------------");
+//Console.WriteLine("Veiculos");
+//Console.WriteLine("-------------------------------------------");
 //Veiculo veiculo01 = new Carro("BMW", "Series 2", 150, 4);
 
 //Console.WriteLine("-------------------------------------------");
@@ -21,23 +22,36 @@ Console.WriteLine("-------------------------------------------");
 // ** Superação **
 // ** Sabendo que, as ações de cada produto são diferentes, crie um método que seja sobrescrito nas classes filhas.
 
-Produto aromatizador = new Aromatizador()
-{
-    Descricao = "Aromatizador de veiculo",
-    Marca = "Cheiro Bom",
-    TipoProduto = "Aromatizador",
-    Utilidade = "Deixar um cheiro bom"
-};
-Produto som = new Som()
-{
-    Descricao = "Fazer um som para o carro",
-    Marca = "Buster",
-    TipoProduto = "Som",
-    Utilidade = "Peso"
-};
+//Produto aromatizador = new Aromatizador()
+//{
+//    Descricao = "Aromatizador de veiculo",
+//    Marca = "Cheiro Bom",
+//    TipoProduto = "Aromatizador",
+//    Utilidade = "Deixar um cheiro bom"
+//};
+//Produto som = new Som()
+//{
+//    Descricao = "Fazer um som para o carro",
+//    Marca = "Buster",
+//    TipoProduto = "Som",
+//    Utilidade = "Peso"
+//};
 
-aromatizador.AcaoProduto();
-som.AcaoProduto();
+//aromatizador.AcaoProduto();
+//som.AcaoProduto();
 
+var pagamento = new Pagamento(2000, "Pix");
+Console.WriteLine(pagamento.ToString());
+
+pagamento.AlterarFormaDePagamento("Cartão de Crédito");
+Console.WriteLine(pagamento.ToString());
+
+pagamento.AlterarValorTotal(2850.90M);
+Console.WriteLine(pagamento.ToString());
+
+pagamento.AlterarFormaDePagamento("Débito");
+Console.WriteLine(pagamento.ToString());
+
+pagamento.EfetuarPagamento();
 
 Console.ReadLine();
---
+++ src/CarCouts.OrientacaoObjeto/Encapsulamento/Pagamento.cs
@@ -0,0 +1,38 @@
+﻿namespace CarCouts.OrientacaoObjeto.Encapsulamento
+{
+    public class Pagamento
+    {
+        public Pagamento(decimal valorTotal, string formaPagamento)
+        {
+            ValorTotal = valorTotal;
+            FormaPagamento = formaPagamento;
+        }
+
+        public decimal ValorTotal { get; private set; }
+        public string FormaPagamento { get; private set; }
+
+        public void AlterarValorTotal(decimal novoValorTotal)
+        {
+            Console.WriteLine($"{DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")} | O Valor total que era {this.ValorTotal}, foi alterado para {novoValorTotal}");
+            this.ValorTotal = novoValorTotal;
+        }
+
+        public void AlterarFormaDePagamento(string novaFormaPagamento)
+        {
+            Console.WriteLine($"{DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")} | A Forma de Pagamento que era {this.FormaPagamento}, foi alterada para {novaFormaPagamento}");
+            this.FormaPagamento = novaFormaPagamento;
+        }
+
+        public void EfetuarPagamento()
+        {
+            Console.WriteLine("--------------------------");
+            Console.WriteLine(ToString());
+            Console.WriteLine($"{DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")} | Pagamento efetuado com sucesso!");
+        }
+
+        public override string ToString()
+        {
+            return $"Valor Total de {this.ValorTotal} e a forma de pagamento será {this.FormaPagamento}";
+        }
+    }
+}
